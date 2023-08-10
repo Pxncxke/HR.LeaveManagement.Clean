5f8a2a05f2417a33b287e00b3bce97ee0da764ca
Erick Aulestia <pxncxke@gmail.com>
2023-08-09 23:44:30 -0500

    add validator and logger for updateleavetypcomandehandler

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommand.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommand.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType
 {
-    internal class UpdateLeaveTypeCommand : IRequest<Unit>
+    public class UpdateLeaveTypeCommand : IRequest<Unit>
     {
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public int DefaultDays { get; set; }
     }
--- HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
 using HR.LeaveManagement.Application.Contracts.Persistance;
+using HR.LeaveManagement.Application.Exceptions;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -14,15 +16,26 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeave
     {
         private readonly IMapper mapper;
         private readonly ILeaveTypeRepository leaveTypeRepository;
+        private readonly IAppLogger<UpdateLeaveTypeCommandHandler> logger;
 
-        public UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        public UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository,
+            IAppLogger<UpdateLeaveTypeCommandHandler> logger)
         {
             this.mapper = mapper;
             this.leaveTypeRepository = leaveTypeRepository;
+            this.logger = logger;
         }
         async Task<Unit> IRequestHandler<UpdateLeaveTypeCommand, Unit>.Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
         {
             //validate
+            var validator = new UpdateLeaveTypeCommandValidator(leaveTypeRepository);
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (validationResult.Errors.Any())
+            {
+                logger.LogWarning("Validation errors in update request for {0} - {1}", nameof(LeaveType), request.Id);
+                throw new BadRequestException("Invalid Leave type", validationResult);
+            }
 
             //convert to domain
             var leaveTypeToUpdate = mapper.Map<Domain.LeaveType>(request);
