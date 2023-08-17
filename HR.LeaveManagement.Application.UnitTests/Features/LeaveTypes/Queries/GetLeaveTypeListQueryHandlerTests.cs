4908f20ff57d23e12521c0cea517fe6f8522824e
Erick Aulestia <pxncxke@gmail.com>
2023-08-16 21:19:54 -0500

    add unit tests, integration tests

---
+++ HR.LeaveManagement.Application.UnitTests/Features/LeaveTypes/Queries/GetLeaveTypeListQueryHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Contracts.Persistance;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManagement.Application.MappingProfiles;
+using HR.LeaveManagement.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.UnitTests.Features.LeaveTypes.Queries
+{
+    public class GetLeaveTypeListQueryHandlerTests
+    {
+        private readonly Mock<ILeaveTypeRepository> mockRepo;
+        private readonly IMapper mapper;
+        private readonly Mock<IAppLogger<GetLeaveTypesQueryHandler>> mockAppLoger;
+
+        public GetLeaveTypeListQueryHandlerTests()
+        {
+            mockRepo = MockLeaveTypeRepository.GetLeaveTypeMockLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(w =>
+            {
+                w.AddProfile<LeaveTypeProfile>();
+            });
+            mapper = mapperConfig.CreateMapper();
+            mockAppLoger = new Mock<IAppLogger<GetLeaveTypesQueryHandler>>();
+        }
+
+        [Fact]
+        public async Task GetLeaveTypeListTest()
+        {
+            var handler = new GetLeaveTypesQueryHandler(mapper,mockRepo.Object, mockAppLoger.Object);
+
+            var result = await handler.Handle(new GetLeaveTypesQuery(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<LeaveTypeDto>>();
+            result.Count.ShouldBe(3);
+        }
+    }
+}
